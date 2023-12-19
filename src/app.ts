7466ef4911e30e193b4048a471af87fb89d32744
Farukul Islam <farukgb1999@gmail.com>
2023-12-19 21:59:20 +0600

    make moduleRoutes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
+import router from './app/router';
 
 const app: Application = express();
 
@@ -12,8 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
   const a = 10;
