0347bc245e0608a427aa4de40c864e0409bbfd07
Farukul Islam <farukgb1999@gmail.com>
2023-12-22 22:46:57 +0600

    comment added

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -5,6 +5,7 @@ type TResponse<T> = {
   message?: string;
   data: T;
 };
+// response function
 const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   res.status(data?.statusCode).json({
     success: data.success,
